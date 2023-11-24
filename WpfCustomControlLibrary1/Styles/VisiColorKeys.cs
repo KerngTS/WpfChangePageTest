1110b52992a54a12683fe0e3cc0728ac7513d5c9
KerngTS <173312871@qq.com>
2023-11-24 15:36:04 +0800

    add visi color

---
+++ WpfCustomControlLibrary1/Styles/VisiColorKeys.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfCustomControlLibrary1.Styles
+{
+    internal class VisiColorKeys
+    {
+        public const string BackgroundColor = "background color";
+        public const string TextColor = "text color";
+        public const string SBTextColor = "sb text color";
+        public const string UILineColor = "ui lines color";
+        public const string SBBackgroundColor = "sb background color";
+        public const string TextHighlightedNonFocusedBackground = "text highlighted non focused background color";
+        public const string WidgetBackground = "widget background color";
+        public const string TabTextColor = "tab text color";
+        public const string TabTextColorWinSelector = "tab text color win selector";
+        public const string TabHighlightedTextColor = "tab highlighted text color";
+        public const string TabHighlightedTextColorWinSelector = "tab highlighted text color win selector";
+        public const string TabNormColor = "tab normal color";
+        public const string TabHighlightedColor = "tab highlighted color";
+        public const string TabSelectedColor = "tab selected color";
+        public const string DockbarCaptionColor = "dockbar caption color";
+        public const string ProptreeButtonColor = "proptree button color";
+        public const string BurshButtonHighlightColor = "brush button highlight color";
+        public const string BurshButtonDownColor = "bursh button down color";
+        public const string PenButtonHighlightColor = "pen button highlight color";
+        public const string PenButtonDownColor = "pen button down color";
+    }
+}
