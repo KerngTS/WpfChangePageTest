3eb867f474e233854c685e4e950e347db484eae3
KerngTS <173312871@qq.com>
2023-12-12 17:23:12 +0800

    DialogParameter add TryGetValue method

--- WpfChangePageTest/Dialog/DialogParameters.cs
+++ WpfChangePageTest/Dialog/DialogParameters.cs
@@ -37,5 +37,102 @@ namespace WpfChangePageTest.Dialog
             }
             return default(T);
         }
+
+        public bool TryGetValue<T>(string key,out T tValue)
+        {
+            var success = false;
+            var type = typeof(T);
+            var value = GetDefault(type);
+            if (this.ContainsKey(key))
+            {
+                value = parameters[key];
+            }
+
+            if  (value == null)
+            {
+                success = true;
+            }
+            else if (value.GetType() == type)
+            {
+                success = true;
+            }
+            else if (type.IsAssignableFrom(value.GetType()))
+            {
+                success = true;
+            }
+            else if (type.IsEnum)
+            {
+                var valueAsString = value.ToString();
+                if (Enum.IsDefined(type, valueAsString))
+                {
+                    success = true;
+                    value = Enum.Parse(type, valueAsString);
+                }
+                else if (int.TryParse(valueAsString, out var numericValue))
+                {
+                    success = true;
+                    value = Enum.ToObject(type, numericValue);
+                }
+            }
+
+            if (!success && type.GetInterface("System.IConvertible") != null)
+            {
+                success = true;
+                value = Convert.ChangeType(value, type);
+            }
+            tValue=(T)value;
+            return success;
+        }
+
+        private static bool TryGetValueInternal(KeyValuePair<string, object> kvp, Type type, out object value)
+        {
+            value = GetDefault(type);
+            var success = false;
+            if (kvp.Value == null)
+            {
+                success = true;
+            }
+            else if (kvp.Value.GetType() == type)
+            {
+                success = true;
+                value = kvp.Value;
+            }
+            else if (type.IsAssignableFrom(kvp.Value.GetType()))
+            {
+                success = true;
+                value = kvp.Value;
+            }
+            else if (type.IsEnum)
+            {
+                var valueAsString = kvp.Value.ToString();
+                if (Enum.IsDefined(type, valueAsString))
+                {
+                    success = true;
+                    value = Enum.Parse(type, valueAsString);
+                }
+                else if (int.TryParse(valueAsString, out var numericValue))
+                {
+                    success = true;
+                    value = Enum.ToObject(type, numericValue);
+                }
+            }
+
+            if (!success && type.GetInterface("System.IConvertible") != null)
+            {
+                success = true;
+                value = Convert.ChangeType(kvp.Value, type);
+            }
+
+            return success;
+        }
+
+        private static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
     }
 }
---
+++ WpfCustomControlLibrary1/KFloatingTextBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCustomControlLibrary1
+{
+    public class KFloatingTextBox:TextBox
+    {
+        static KFloatingTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(KFloatingTextBox), new FrameworkPropertyMetadata(typeof(KFloatingTextBox)));
+        }
+
+
+
+        public string FloatingText
+        {
+            get { return (string)GetValue(FloatingTextProperty); }
+            set { SetValue(FloatingTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FloatingText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FloatingTextProperty =
+            DependencyProperty.Register("FloatingText", typeof(string), typeof(KFloatingTextBox), new PropertyMetadata(default(string)));
+
+
+    }
+}
