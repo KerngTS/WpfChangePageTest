f16c2f41d2023277a89b514641cf98fd0f940623
KerngTS <173312871@qq.com>
2023-12-14 18:06:33 +0800

    Visual host base

---
+++ WpfCustomControlLibrary1/KVisualHostBase.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace WpfCustomControlLibrary1
+{
+    public class KVisualHostBase:FrameworkElement
+    {
+        /// <summary>
+        /// 图像容器
+        /// </summary>
+        private VisualCollection _children;
+        /// <summary>
+        /// 默认字体
+        /// </summary>
+        protected Typeface DefaultTypeFace => new Typeface("Verdana");
+        /// <summary>
+        /// 绘图区域的宽：X
+        /// </summary>
+        protected double DrawingRegionWidth { get; set; } = 200;
+        /// <summary>
+        /// 绘图区域的高：Y
+        /// </summary>
+        protected double DrawingRegionHeight { get; set; } = 200;
+        /// <summary>
+        /// X
+        /// 水平方向
+        /// 像素比
+        /// 1单位数据对应多少像素
+        /// 根据实际场景宽度和控件宽度得到
+        /// </summary>
+        protected double XPixelRatio { get; set; } = 10;
+        /// <summary>
+        /// Y
+        /// 垂直方向
+        /// 像素比
+        /// 1单位数据对应多少像素
+        /// 根据实际场景长(高)度和控件高度得到
+        /// </summary>
+        protected double YPixelRatio { get; set; } = 10;
+
+        public KVisualHostBase()
+        {
+            _children = new VisualCollection(this); 
+        }
+
+
+        public Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Background.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(KVisualHostBase), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        /// <summary>
+        /// Y轴最大值
+        /// </summary>
+        public double YMax
+        {
+            get { return (double)GetValue(YMaxProperty); }
+            set { SetValue(YMaxProperty, value); }
+        }
+        private static readonly DependencyProperty YMaxProperty =
+            DependencyProperty.Register("YMax", typeof(double), typeof(KVisualHostBase),
+                new FrameworkPropertyMetadata(100D, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(YRangeChanged)));
+
+        /// <summary>
+        /// Y轴最小值
+        /// </summary>
+        public double YMin
+        {
+            get { return (double)GetValue(YMinProperty); }
+            set { SetValue(YMinProperty, value); }
+        }
+        public static readonly DependencyProperty YMinProperty =
+            DependencyProperty.Register("YMin", typeof(double), typeof(KVisualHostBase),
+                new FrameworkPropertyMetadata(-100D, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(YRangeChanged)));
+
+        /// <summary>
+        /// X轴最大值
+        /// </summary>
+        public double XMax
+        {
+            get { return (double)GetValue(XMaxProperty); }
+            set { SetValue(XMaxProperty, value); }
+        }
+        public static readonly DependencyProperty XMaxProperty =
+            DependencyProperty.Register("XMax", typeof(double), typeof(KVisualHostBase),
+                new FrameworkPropertyMetadata(100D, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(XRangeChanged)));
+
+        /// <summary>
+        /// X轴最小值
+        /// </summary>
+        public double XMin
+        {
+            get { return (double)GetValue(XMinProperty); }
+            set { SetValue(XMinProperty, value); }
+        }
+        public static readonly DependencyProperty XMinProperty =
+            DependencyProperty.Register("XMin", typeof(double), typeof(KVisualHostBase),
+                new FrameworkPropertyMetadata(-100D,FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,new PropertyChangedCallback(XRangeChanged)));
+
+        private static void YRangeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is KVisualHostBase g)
+            {
+                g.UpdatePixelRatio();
+            }
+        }
+
+        private static void XRangeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is KVisualHostBase g)
+            {
+                g.UpdatePixelRatio();
+            }
+        }
+
+        protected void Add(DrawingVisual drawingVisual) => _children.Add(drawingVisual);
+
+        /// <summary>
+        /// 转换实际坐标的Y点为图像坐标值
+        /// </summary>
+        /// <param name="actY"></param>
+        /// <returns></returns>
+        protected double YAxisConvertYToPixel(double actY)
+        {
+            var orgY = actY - YMin;
+            var orgYToPX = RenderSize.Height - orgY * YPixelRatio;
+            return orgYToPX;
+        }
+
+        /// <summary>
+        /// 转换实际坐标的X点为图像坐标值
+        /// 水平坐标轴（X轴）值转换，X
+        /// 将实际值转换成像素
+        /// </summary>
+        /// <param name="actX"></param>
+        /// <returns></returns>
+        protected double XAxisConvertXToPixel(double actX)
+        {
+            var orgX = actX - XMin;
+            var orgXToPX = orgX * XPixelRatio;
+            return orgXToPX;
+        }
+
+        protected double YAxisConvertToActual(double pixelY)
+        {
+            var cur = (RenderSize.Height - pixelY) / YPixelRatio;
+            return cur + YMin;
+        }
+
+        protected double XAxisConvertToActual(double pixelX)
+        {
+            var cur = (pixelX) / YPixelRatio;
+            return cur + XMin;
+        }
+
+        /// <summary>
+        /// 坐标点转换
+        /// </summary>
+        /// <param name="actP"></param>
+        /// <returns></returns>
+        protected Point ConvertToPixel(Point actP)
+        {
+            var pt = new Point();
+            pt.X = XAxisConvertXToPixel(actP.X);
+            pt.Y = YAxisConvertYToPixel(actP.Y);
+            return pt;
+        }
+
+        protected Point ConvertToActual(Point pixcel)
+        {
+            var pt = new Point();
+            pt.X = XAxisConvertToActual(pixcel.X);
+            pt.Y = YAxisConvertToActual(pixcel.Y);
+            return pt;
+        }
+
+        /// <summary>
+        /// 更新像素比
+        /// </summary>
+        protected void UpdatePixelRatio()
+        {
+            // 场景实际大小情况
+            DrawingRegionWidth = XMax - XMin;
+            DrawingRegionHeight = YMax - YMin;
+
+            // 实际场景中1m对应多少像素点
+            XPixelRatio = RenderSize.Width / DrawingRegionWidth;
+            YPixelRatio = RenderSize.Height / DrawingRegionHeight;
+        }
+
+
+        protected override int VisualChildrenCount => _children.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _children[index];
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            UpdatePixelRatio();
+            // 显示图形
+            //InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(Background, null, new Rect(0, 0, RenderSize.Width, RenderSize.Height));
+            base.OnRender(drawingContext);
+        }
+    }
+}
---
+++ WpfCustomControlLibrary1/VVisualHost.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfCustomControlLibrary1
+{
+    public class VVisualHost : KVisualHostBase
+    {
+        //創建一個容器
+        //private VisualCollection _children;
+        DrawingVisual RecVisual;
+        public VVisualHost()
+        {
+            //_children = new VisualCollection(this);
+            RecVisual = new DrawingVisual();
+            //_children.Add(CreateDrawingVisualRectangle());
+            Add(RecVisual);
+            //_children.Add(CreateDrawingVisualEllipses());
+            //_children.Add(CreateDrawingVisualPath());
+
+            this.MouseLeftButtonUp += VVisualHost_MouseLeftButtonUp;
+
+            //System.Collections.Generic.ICollection<Typeface> typefaces = Fonts.GetTypefaces("file:///C:\\Windows\\Fonts\\#Georgia");
+
+            //// Enumerate the typefaces in the collection.
+            //foreach (Typeface face in typefaces)
+            //{
+            //    // Separate the URI directory source info from the font family name.
+            //    string[] familyName = face.FontFamily.Source.Split('#');
+
+            //    // Add the font family name, weight, and style values to the typeface combo box.
+            //    var rr = familyName[familyName.Length - 1] + " " + face.Weight + " " + face.Style;
+            //}
+        }
+
+
+
+        private void VVisualHost_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Point pt = e.GetPosition((UIElement)sender);
+            VisualTreeHelper.HitTest(this, null, new HitTestResultCallback(MyCallBack), new PointHitTestParameters(pt));
+        }
+
+        private HitTestResultBehavior MyCallBack(HitTestResult result)
+        {
+            if (result.VisualHit.GetType() == typeof(DrawingVisual))
+            {
+                if (((DrawingVisual)result.VisualHit).Opacity == 1.0)
+                {
+                    ((DrawingVisual)result.VisualHit).Opacity = 0.4;
+                }
+                else
+                {
+                    ((DrawingVisual)result.VisualHit).Opacity = 1.0;
+                }
+            }
+            return HitTestResultBehavior.Stop;
+        }
+
+        private DrawingVisual CreateDrawingVisualEllipses()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            drawingContext.DrawEllipse(Brushes.SkyBlue, null, new Point(320, 140), 160, 40);
+            drawingContext.Close();
+            return drawingVisual;
+        }
+
+        private DrawingVisual CreateDrawingVisualPath()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            //PathFigure pathFigure = new PathFigure();
+
+            PathGeometry myPathGeometry = new PathGeometry();
+
+            // Create a figure.
+            PathFigure pathFigure1 = new PathFigure();
+            //pathFigure1.StartPoint = new Point(105, 100);
+            //PolyLineSegment myPolyLineSegment=new PolyLineSegment(new List<Point>()
+            //{
+            //    /*new Point(105,100),*/new Point(295,100),new Point(300,105),new Point(300,105),new Point(300,195),new Point(295,200),new Point(105,200),new Point(100,195),new Point(100,105),new Point(105,100)
+            //},true);
+
+            //pathFigure1.Segments.Add(myPolyLineSegment);
+
+            pathFigure1.StartPoint = ConvertToPixel(new Point(100, 105));
+            pathFigure1.Segments.Add(
+            new ArcSegment(
+                ConvertToPixel(new Point(105, 100)),
+                new Size(5, 5),
+                0,
+                false, /* IsLargeArc */
+                SweepDirection.Clockwise,
+                true /* IsStroked */ ));
+            pathFigure1.Segments.Add(new LineSegment(ConvertToPixel(new Point(295, 100)), true));
+
+            pathFigure1.Segments.Add(
+            new ArcSegment(
+                ConvertToPixel(new Point(300, 105)),
+                new Size(5, 5),
+                0,
+                false, /* IsLargeArc */
+                SweepDirection.Clockwise,
+                true /* IsStroked */ ));
+            pathFigure1.Segments.Add(new LineSegment(ConvertToPixel(new Point(300, 195)), true));
+
+            pathFigure1.Segments.Add(
+            new ArcSegment(
+                ConvertToPixel(new Point(295, 200)),
+                new Size(5, 5),
+                0,
+                false, /* IsLargeArc */
+                SweepDirection.Clockwise,
+                true /* IsStroked */ ));
+            pathFigure1.Segments.Add(new LineSegment(ConvertToPixel(new Point(105, 200)), true));
+
+            pathFigure1.Segments.Add(
+            new ArcSegment(
+               ConvertToPixel(new Point(100, 195)),
+                new Size(5, 5),
+                0,
+                false, /* IsLargeArc */
+                SweepDirection.Clockwise,
+                true /* IsStroked */ ));
+            pathFigure1.Segments.Add(new LineSegment(ConvertToPixel( new Point(100, 105)), true));
+
+            myPathGeometry.Figures.Add(pathFigure1);
+
+            //pathFigure.StartPoint = new Point(10, 100);
+            //PointCollection myPointCollection = new PointCollection(6);
+            //myPointCollection.Add(new Point(0, 0));
+            //myPointCollection.Add(new Point(200, 0));
+            //myPointCollection.Add(new Point(300, 100));
+            //myPointCollection.Add(new Point(300, 0));
+            //myPointCollection.Add(new Point(400, 0));
+            //myPointCollection.Add(new Point(600, 100));
+
+            //PolyQuadraticBezierSegment myBezierSegment = new PolyQuadraticBezierSegment();
+            //PolyBezierSegment myBezierSegment = new PolyBezierSegment();
+            //myBezierSegment.Points = myPointCollection;
+            //pathFigure1.Segments.Add(myBezierSegment);
+            //pathFigure1.Segments.Add(arcSegment1);
+            //myPathGeometry.Figures.Add(pathFigure1);
+
+            // 使用 DrawGeometry 方法绘制定义的复杂路径
+            drawingContext.DrawGeometry(Brushes.Green, new Pen(Brushes.Red, 1.0), myPathGeometry);
+
+            drawingContext.Close();
+
+            return drawingVisual;
+        }
+
+        private DrawingVisual CreateDrawingVisualPath1()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            PathGeometry myPathGeometry = new PathGeometry();
+
+            PathFigure pathFigure1 = new PathFigure();
+            pathFigure1.StartPoint = new Point(0, 0);
+            ArcSegment arcSegment1 = new ArcSegment(new Point(100, 100), new Size(50, 25), 0, false, SweepDirection.Clockwise, false);
+            pathFigure1.Segments.Add(arcSegment1);
+            myPathGeometry.Figures.Add(pathFigure1);
+
+            // 使用 DrawGeometry 方法绘制定义的复杂路径
+            drawingContext.DrawGeometry(null, new Pen(Brushes.Red, 1.0), myPathGeometry);
+
+            drawingContext.Close();
+
+            return drawingVisual;
+        }
+
+        private DrawingVisual CreateDrawingVisualText()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            var pt = new Point(0, 100);
+            var text = new FormattedText("冲床钢", CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 50, Brushes.SeaGreen, VisualTreeHelper.GetDpi(this).PixelsPerDip);
+            var geo = text.BuildGeometry(pt);
+            var pathGeo = geo.GetOutlinedPathGeometry();
+            pathGeo.FillRule = FillRule.EvenOdd;
+            drawingContext.DrawGeometry(Brushes.Green, new Pen(Brushes.Red, 1), geo);
+            //drawingContext.DrawText(text, pt);
+            drawingContext.Close();
+            return drawingVisual;
+        }
+
+        private DrawingVisual CreateDrawingVisualRectangle()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            Rect rect = new Rect(ConvertToPixel(new Point(100, 100)), new Size(100, 80));
+            drawingContext.DrawRectangle(Brushes.Red, null, rect);
+            drawingContext.Close();
+            return drawingVisual;
+        }
+
+        //protected override int VisualChildrenCount => _children.Count;
+
+        //protected override Visual GetVisualChild(int index)
+        //{
+        //    if (index < 0 || index >= _children.Count)
+        //        throw new ArgumentOutOfRangeException("index");
+        //    return _children[index];
+        //}
+
+        private void Draw()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var dc = RecVisual.RenderOpen();
+                var p1 = ConvertToPixel(new Point(-25, -25));
+                var p2 = ConvertToPixel(new Point(25, 25));
+                var w  = p2.X - p1.X;
+                var h = p2.Y - p1.Y;
+
+                Rect rect = new Rect(p1, p2);
+                dc.DrawRectangle(Brushes.Red, null, rect);
+                dc.Close();
+                // 显示图形
+                InvalidateVisual();
+            });
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            Draw();
+        }
+    }
+}
