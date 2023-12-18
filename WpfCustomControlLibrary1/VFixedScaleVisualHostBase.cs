68c026b85f418c915857dadd5d84acea5fc0bab8
KerngTS <173312871@qq.com>
2023-12-18 18:47:31 +0800

    update point drawer host

---
+++ WpfCustomControlLibrary1/VFixedScaleVisualHostBase.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Xml.Schema;
+
+namespace WpfCustomControlLibrary1
+{
+    public class VFixedScaleVisualHostBase : FrameworkElement
+    {
+        /// <summary>
+        /// 图像容器
+        /// </summary>
+        private VisualCollection _children;
+        //private DrawingVisual _backVisual;
+        protected double mScale;
+        protected double xMin;
+        protected double xMax;
+        protected double yMin;
+        protected double yMax;
+        /// <summary>
+        /// 默认字体
+        /// </summary>
+        protected Typeface DefaultTypeFace => new Typeface("Verdana");
+
+        public VFixedScaleVisualHostBase()
+        {
+            _children = new VisualCollection(this);
+            //_backVisual = new DrawingVisual();
+            //Add(_backVisual);
+        }
+
+
+        public Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Background.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(KVisualHostBase),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(RegionChanged)));
+
+
+
+        public double RegionWidth
+        {
+            get { return (double)GetValue(RegionWidthProperty); }
+            set { SetValue(RegionWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RegionWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RegionWidthProperty =
+            DependencyProperty.Register("RegionWidth", typeof(double), typeof(VFixedScaleVisualHostBase),
+                new FrameworkPropertyMetadata(200.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(RegionChanged)));
+
+        public double RegionHeight
+        {
+            get { return (double)GetValue(RegionHeightProperty); }
+            set { SetValue(RegionHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RegionWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RegionHeightProperty =
+            DependencyProperty.Register("RegionHeight", typeof(double), typeof(VFixedScaleVisualHostBase),
+                new FrameworkPropertyMetadata(100.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(RegionChanged)));
+
+        private static void RegionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var v = d as VFixedScaleVisualHostBase;
+            v.UpdatePixelRatio();
+            //v.Draw();
+        }
+
+        protected void Add(DrawingVisual drawingVisual) => _children.Add(drawingVisual);
+
+        /// <summary>
+        /// 转换实际坐标的Y点为图像坐标值
+        /// </summary>
+        /// <param name="actY"></param>
+        /// <returns></returns>
+        protected double YAxisConvertYToPixel(double actY)
+        {
+            var orgY = actY - yMin;
+            var orgYToPX = RenderSize.Height - orgY * mScale;
+            return orgYToPX;
+        }
+
+        /// <summary>
+        /// 转换实际坐标的X点为图像坐标值
+        /// 水平坐标轴（X轴）值转换，X
+        /// 将实际值转换成像素
+        /// </summary>
+        /// <param name="actX"></param>
+        /// <returns></returns>
+        protected double XAxisConvertXToPixel(double actX)
+        {
+            var orgX = actX - xMin;
+            var orgXToPX = orgX * mScale;
+            return orgXToPX;
+        }
+
+        protected Point ConvertToPixel(Point actP)
+        {
+            var pt = new Point();
+            pt.X = XAxisConvertXToPixel(actP.X);
+            pt.Y = YAxisConvertYToPixel(actP.Y);
+            return pt;
+        }
+
+        
+
+        protected override int VisualChildrenCount => _children.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _children[index];
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            UpdatePixelRatio();
+            // 显示图形
+            //InvalidateVisual();
+        }
+
+        private void UpdatePixelRatio()
+        {
+            var xRate = RegionWidth/RenderSize.Width;
+            var yRate = RegionHeight/RenderSize.Height;
+            var maxRate = xRate>yRate ? xRate : yRate;
+            var width = RenderSize.Width * maxRate;
+            var height = RenderSize.Height * maxRate;
+            var halfWidth = width / 2.0;
+            var halfHeight = height / 2.0;
+            xMin=-halfWidth;
+            xMax=halfWidth;
+            yMin=-halfHeight;
+            yMax=halfHeight;
+            mScale = 1.0 / maxRate;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            //Draw();
+            drawingContext.DrawRectangle(Background, null, new Rect(0, 0, RenderSize.Width, RenderSize.Height));
+            base.OnRender(drawingContext);
+        }
+
+        //private void Draw()
+        //{
+        //    this.Dispatcher.Invoke(() =>
+        //    {
+        //        var dc = _backVisual.RenderOpen();
+
+        //        dc.DrawRectangle(Background, null, new Rect(0, 0, RenderSize.Width, RenderSize.Height));
+
+        //        dc.Close();
+        //        // 显示图形
+        //        //InvalidateVisual();
+        //    });
+        //}
+    }
+}
---
+++ WpfCustomControlLibrary1/VPointArrayDrawerHost.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace WpfCustomControlLibrary1
+{
+    public class VPointArrayDrawerHost : VFixedScaleVisualHostBase
+    {
+        private DrawingVisual _visual;
+
+        public VPointArrayDrawerHost()
+        {
+            _visual = new DrawingVisual();
+            Add(_visual);
+        }
+
+        #region 依賴屬性
+
+
+        public double ReviewWidth
+        {
+            get { return (double)GetValue(ReviewWidthProperty); }
+            set { SetValue(ReviewWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ReviewWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ReviewWidthProperty =
+            DependencyProperty.Register("ReviewWidth", typeof(double), typeof(VPointArrayDrawerHost),
+                new FrameworkPropertyMetadata(200.0,FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,new PropertyChangedCallback(HostCallBack),new CoerceValueCallback(ReviewWidthCoerceValue)));
+
+        private static object ReviewWidthCoerceValue(DependencyObject d, object baseValue)
+        {
+            return baseValue;
+            var self = d as VPointArrayDrawerHost;
+            var newValue = (double)baseValue;
+            var xRate = newValue / self.Width * 1.1;
+            var yRate = self.ReviewHeight / self.Height * 1.1;
+            var maxRate = xRate>yRate ? xRate : yRate;
+            return newValue/maxRate;
+        }
+
+        public double ReviewHeight
+        {
+            get { return (double)GetValue(ReviewHeightProperty); }
+            set { SetValue(ReviewHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ReviewHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ReviewHeightProperty =
+            DependencyProperty.Register("ReviewHeight", typeof(double), typeof(VPointArrayDrawerHost), 
+                new FrameworkPropertyMetadata(150.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack), new CoerceValueCallback(ReviewHeightCoerceValue)));
+
+        private static object ReviewHeightCoerceValue(DependencyObject d, object baseValue)
+        {
+            return baseValue;
+            var self = d as VPointArrayDrawerHost;
+            var newValue = (double)baseValue;
+            var xRate = self.ReviewWidth / self.Width * 1.1;
+            var yRate = newValue / self.Height * 1.1;
+            var maxRate = xRate > yRate ? xRate : yRate;
+            return newValue / maxRate;
+        }
+
+        public double ReviewDiameter
+        {   
+            get { return (double)GetValue(ReviewDiameterProperty); }
+            set { SetValue(ReviewDiameterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ReviewDiameter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ReviewDiameterProperty =
+            DependencyProperty.Register("ReviewDiameter", typeof(double), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(10.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
+
+
+
+        public ObservableCollection<Point> Points
+        {
+            get { return (ObservableCollection<Point>)GetValue(PointsProperty); }   
+            set { SetValue(PointsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Points.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.Register("Points", typeof(ObservableCollection<Point>), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(new ObservableCollection<Point>(), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
+
+
+
+        public Brush EdgeBorderBrush
+        {
+            get { return (Brush)GetValue(EdgeBorderBrushProperty); }
+            set { SetValue(EdgeBorderBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EdgeBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EdgeBorderBrushProperty =
+            DependencyProperty.Register("EdgeBorderBrush", typeof(Brush), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(default(Brush),FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
+
+
+
+        public Brush CircleBorderBrush
+        {
+            get { return (Brush)GetValue(CircleBorderBrushProperty); }
+            set { SetValue(CircleBorderBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CircleBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CircleBorderBrushProperty =
+            DependencyProperty.Register("CircleBorderBrush", typeof(Brush), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
+
+
+
+        private static void HostCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var self = d as VPointArrayDrawerHost;
+            self.RegionWidth = self.ReviewWidth*1.1;
+            self.RegionHeight=self.ReviewHeight*1.1;
+
+            self.Draw();
+        }
+
+
+        #endregion
+
+        private  IList<Point> GetDefaultPoints()
+        {
+            var result = new List<Point>();
+            result.Add(new Point(0, 0));
+
+            var x = ReviewWidth / 2.0 - 20.0;
+            var y = ReviewHeight / 2.0 - 20.0;
+            result.Add(new Point(x, y));
+            result.Add(new Point(-x, -y));
+            result.Add(new Point(-x, y));
+            result.Add(new Point(x, -y));
+
+
+            return result;
+        }
+
+        private void Draw()
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var dc = _visual.RenderOpen();
+                
+                dc.DrawRectangle(null, new Pen(EdgeBorderBrush,1.0), GetRect());
+                var pts = GetDefaultPoints();
+                //Points = new ObservableCollection<Point>(GetDefaultPoints());
+                foreach (var pt in pts)
+                {
+                    var center = ConvertToPixel(pt);
+                    dc.DrawEllipse(null, new Pen(CircleBorderBrush, 1.0), center, mScale * ReviewDiameter / 2.0, mScale * ReviewDiameter / 2.0);
+                }
+                dc.Close();
+                // 显示图形
+                InvalidateVisual();
+            });
+        }
+
+        public Rect GetRect()
+        {//繪制預覽模板外形
+            var halfWidth = ReviewWidth / 2.0;
+            var halfHeight = ReviewHeight / 2.0;
+            var p1 = ConvertToPixel(new Point(0.0 - halfWidth, 0.0 - halfHeight));
+            var p2 = ConvertToPixel(new Point(halfWidth, halfHeight));
+            Rect rect = new Rect(p1, p2);
+            return rect;
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            Draw();
+        }
+    }
+}
