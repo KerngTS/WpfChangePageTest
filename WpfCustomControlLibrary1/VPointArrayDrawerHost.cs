dbe9c610105beb42110a517ae3609c8bea0efc99
KerngTS <173312871@qq.com>
2023-12-22 15:41:03 +0800

    add spline

--- WpfCustomControlLibrary1/VPointArrayDrawerHost.cs
+++ WpfCustomControlLibrary1/VPointArrayDrawerHost.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 using WpfCustomControlLibrary1.Tools;
 
 namespace WpfCustomControlLibrary1
@@ -21,6 +22,7 @@ namespace WpfCustomControlLibrary1
         {
             _visual = new DrawingVisual();
             Add(_visual);
+            SnapsToDevicePixels=true;
         }
 
         #region 依賴屬性
@@ -140,6 +142,17 @@ namespace WpfCustomControlLibrary1
             DependencyProperty.Register("CircleBorderBrush", typeof(Brush), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
 
 
+        public Brush SplineBrush
+        {
+            get { return (Brush)GetValue(SplineBrushProperty); }
+            set { SetValue(SplineBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CircleBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SplineBrushProperty =
+            DependencyProperty.Register("SplineBrush", typeof(Brush), typeof(VPointArrayDrawerHost), new FrameworkPropertyMetadata(Brushes.Black, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(HostCallBack)));
+
+
 
         private static void HostCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -190,15 +203,162 @@ namespace WpfCustomControlLibrary1
                 //DrawDimension(dc, p1, ConvertToPixel(new Point(halfWidth, 0.0 - halfHeight)), ConvertToPixel( new Point(0, 0.0 - halfHeight)), "DIMENSION");
                 DrawDimension(dc, new Point(0.0 - halfWidth, 0.0 - halfHeight), new Point(0,0), new Point(0.0 - halfWidth/2.0, 0.0 - halfHeight-30), "HDIMENSION",0);
                 DrawDimension(dc, new Point(0.0 - halfWidth, 0.0 - halfHeight), new Point(0,0), new Point(0.0 + halfWidth+30, 0.0 - halfHeight/2.0), "VDIMENSION",1);
+                DrawSpline(dc);
                 dc.Close();
                 // 显示图形
                 InvalidateVisual();
             });
         }
 
+        public void DrawSpline(DrawingContext dc)
+        {
+            //var gsc = new GradientStopCollection();
+            //gsc.Add(new GradientStop(Colors.Blue, 0));
+            //gsc.Add(new GradientStop(Colors.Green, 0.5));
+            //gsc.Add(new GradientStop(Colors.Yellow, 1));
+            //var brush = new LinearGradientBrush(gsc);
+            var pen = new Pen(SplineBrush, 3);
+
+            /*
+            //X軸
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, -60)), ConvertToPixel(new Point(80, -60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, -40)), ConvertToPixel(new Point(80, -40)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, -20)), ConvertToPixel(new Point(80, -20)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, 0)), ConvertToPixel(new Point(80, 0)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, 20)), ConvertToPixel(new Point(80, 20)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, 40)), ConvertToPixel(new Point(80, 40)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, 60)), ConvertToPixel(new Point(80, 60)));
+
+            //Y軸
+            dc.DrawLine(pen, ConvertToPixel(new Point(-80, -60)), ConvertToPixel(new Point(-80, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-60, -60)), ConvertToPixel(new Point(-60, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-40, -60)), ConvertToPixel(new Point(-40, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(-20, -60)), ConvertToPixel(new Point(-20, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(0, -60)), ConvertToPixel(new Point(0, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(20, -60)), ConvertToPixel(new Point(20, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(40, -60)), ConvertToPixel(new Point(40, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(60, -60)), ConvertToPixel(new Point(60, 60)));
+            dc.DrawLine(pen, ConvertToPixel(new Point(80, -60)), ConvertToPixel(new Point(80, 60)));
+            */
+
+            //PolyQuadraticBezier
+            var points = new[]
+            {
+                ConvertToPixel( new Point(-80, -60)),
+                ConvertToPixel(new Point(-60, 60)),
+                ConvertToPixel( new Point(-40, -60)),
+                ConvertToPixel(new Point(-20, 60)),
+                ConvertToPixel( new Point(0, -60)),
+                ConvertToPixel(new Point(20, 60)),
+                ConvertToPixel( new Point(40, -60)),
+                ConvertToPixel(new Point(60, 60)),
+                ConvertToPixel(new Point(80, -60)),
+            };
+
+            
+            var path = MakeCurve(points,1.0);
+
+
+            //PathGeometry myPathGeometry = new PathGeometry();
+            //myPathGeometry.Figures .Add();
+
+            // 使用 DrawGeometry 方法绘制定义的复杂路径
+            dc.DrawGeometry(null,pen, (PathGeometry)path.Data);
+        }
+
+        private Path MakeCurve(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            Point[] result_points = MakeCurvePoints(points, tension);
+
+            // Use the points to create the path.
+            return MakeBezierPath(result_points.ToArray());
+        }
+
+
+        private Point[] MakeCurvePoints(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            double control_scale = tension / 0.5 * 0.175;
+
+            List<Point> result_points = new List<Point>();
+            result_points.Add(points[0]);
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                // Get the point and its neighbors.
+                Point pt_before = points[Math.Max(i - 1, 0)];
+                Point pt = points[i];
+                Point pt_after = points[i + 1];
+                Point pt_after2 = points[Math.Min(i + 2, points.Length - 1)];
+
+                double dx1 = pt_after.X - pt_before.X;
+                double dy1 = pt_after.Y - pt_before.Y;
+
+                Point p1 = points[i];
+                Point p4 = pt_after;
+
+                double dx = pt_after.X - pt_before.X;
+                double dy = pt_after.Y - pt_before.Y;
+                Point p2 = new Point(
+                    pt.X + control_scale * dx,
+                    pt.Y + control_scale * dy);
+
+                dx = pt_after2.X - pt.X;
+                dy = pt_after2.Y - pt.Y;
+                Point p3 = new Point(
+                    pt_after.X - control_scale * dx,
+                    pt_after.Y - control_scale * dy);
+
+                // Save points p2, p3, and p4.
+                result_points.Add(p2);
+                result_points.Add(p3);
+                result_points.Add(p4);
+            }
+
+            // Return the points.
+            return result_points.ToArray();
+        }
+
+        private Path MakeBezierPath(Point[] points)
+        {
+            // Create a Path to hold the geometry.
+            Path path = new Path();
+
+            // Add a PathGeometry.
+            PathGeometry path_geometry = new PathGeometry();
+            path.Data = path_geometry;
+
+            // Create a PathFigure.
+            PathFigure path_figure = new PathFigure();
+            path_geometry.Figures.Add(path_figure);
+
+            // Start at the first point.
+            path_figure.StartPoint = points[0];
+
+            // Create a PathSegmentCollection.
+            PathSegmentCollection path_segment_collection =
+                new PathSegmentCollection();
+            path_figure.Segments = path_segment_collection;
+
+            // Add the rest of the points to a PointCollection.
+            PointCollection point_collection =
+                new PointCollection(points.Length - 1);
+            for (int i = 1; i < points.Length; i++)
+                point_collection.Add(points[i]);
+
+            // Make a PolyBezierSegment from the points.
+            PolyBezierSegment bezier_segment = new PolyBezierSegment();
+            bezier_segment.Points = point_collection;
+
+            // Add the PolyBezierSegment to othe segment collection.
+            path_segment_collection.Add(bezier_segment);
+
+            return path;
+        }
         //public Rect GetRect()
         //{//繪制預覽模板外形
-            
+
         //    return rect;
         //}
 
